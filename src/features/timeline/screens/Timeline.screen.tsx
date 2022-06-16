59dc3e48ca5f1be46982df7d47d3996f58569331
Bunyasit Fang <wachaa1319@gmail.com>
2022-06-16 20:57:06 +0700

    timeline

--- src/features/timeline/screens/Timeline.screen.tsx
+++ src/features/timeline/screens/Timeline.screen.tsx
@@ -2,59 +2,9 @@ import React from "react";
 import { Image, Pressable } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { RootTabScreenProps } from "../../../../types";
-import { Text, View } from "../../../components/Themed";
+import { Text, View } from "../../../components/styles/Themed";
+import { BandData } from "../components/BandData.component";
 
-function BandData({
-  image,
-  name,
-  time,
-  navigation,
-}: {
-  image: string;
-  name: string;
-  time: Date;
-  navigation: any;
-}) {
-  console.log(new Date().getTime() > time.getTime());
-  console.log(typeof time);
-  console.log(Object.keys(time));
-
-  return (
-    <View style={{ flexDirection: "row", alignItems: "center", margin: 20 }}>
-      <View
-        darkColor={new Date().getTime() > time.getTime() ? "red" : "#444"}
-        style={{
-          padding: 5,
-          borderRadius: 10,
-          height: 30,
-        }}
-      >
-        <Text>{time.toLocaleTimeString()}</Text>
-      </View>
-      <Pressable onPress={() => navigation.toggleDrawer()}>
-        <View
-          darkColor="#333"
-          style={{
-            padding: 10,
-            borderRadius: 10,
-            marginLeft: 30,
-          }}
-        >
-          <View
-            style={{
-              padding: 10,
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <Image source={{ uri: image }} style={{ width: 50, height: 50 }} />
-            <Text>{name}</Text>
-          </View>
-        </View>
-      </Pressable>
-    </View>
-  );
-}
 export default function TimelineScreen({
   navigation,
 }: RootTabScreenProps<"Timeline">) {
