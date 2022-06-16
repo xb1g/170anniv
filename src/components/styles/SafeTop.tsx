8001523410ef1db59aac62f140d4798922169d4c
Bunyasit Fang <wachaa1319@gmail.com>
2022-06-16 20:40:44 +0700

    safetop

--- src/components/styles/SafeTop.tsx
+++ src/components/styles/SafeTop.tsx
@@ -1,36 +1,46 @@
+import { Platform } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { View, ViewProps } from "./Themed";
 
 export function SafeTop(props: ViewProps) {
-  const insets = useSafeAreaInsets();
-  const { style, lightColor, darkColor, ...otherProps } = props;
-  return (
-    <View
-      style={[
-        {
-          height: insets.top,
-          backgroundColor: "transparent",
-        },
-        style,
-      ]}
-      {...otherProps}
-    />
-  );
+  if (Platform.OS === "web") {
+    return <View {...props} />;
+  } else {
+    const insets = useSafeAreaInsets();
+    console.log("SafeTop insets:", insets);
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    return (
+      <View
+        style={[
+          {
+            height: insets.top,
+            backgroundColor: "transparent",
+          },
+          style,
+        ]}
+        {...otherProps}
+      />
+    );
+  }
 }
 
 export function SafeBottom(props: ViewProps) {
-  const insets = useSafeAreaInsets();
-  const { style, lightColor, darkColor, ...otherProps } = props;
-  return (
-    <View
-      style={[
-        {
-          height: insets.bottom,
-          backgroundColor: "transparent",
-        },
-        style,
-      ]}
-      {...otherProps}
-    />
-  );
+  if (Platform.OS === "web") {
+    return <View {...props} />;
+  } else {
+    const insets = useSafeAreaInsets();
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    return (
+      <View
+        style={[
+          {
+            height: insets.bottom,
+            backgroundColor: "transparent",
+          },
+          style,
+        ]}
+        {...otherProps}
+      />
+    );
+  }
 }
